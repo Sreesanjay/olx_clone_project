c9bca2a81be42f023418d7649a46df62d4741390
Sreesanjay <sreesanjay7592@gmail.com>
2023-12-03 12:10:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css"
+export default function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  )
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import "./NavBar.css"
+export default function NavBar() {
+  return (
+    <header className="nav-bar">
+      <img className='olx-logo' src="OLX-Logo-PNG_h0bmj9.png" alt="" />
+
+    </header>
+  )
+}
